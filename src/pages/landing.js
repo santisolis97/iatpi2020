2f92aeb78fac3d88b32c28a533001d3891255bea
Santiago <santux09@gmail.com>
2020-09-22 21:10:40 -0300

    Llamadas a la api funcionando, listo para graficar

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -1,22 +1,74 @@
 import React from 'react'
-import Chart from '../components/Chart.js'
+import Charts from '../components/Chart.js'
 import './landing.css'
-import Form from '../components/Form'
+import axios from 'axios';
 
-function App() {
+
+class App extends React.Component {
+
+  componentDidMount() {
+    axios.get(`http://localhost:8080/api/ia-knn/v1/all`)
+    .then(res => {
+      const all = res.data; 
+      const result = [all.length]
+      this.setState(result);
+      console.log(this.state)
+    })
+  }
+  state = {
+    valores:[]
+  }
+constructor() {
+  super();
+  this.handleSubmit = this.handleSubmit.bind(this);
+}
+
+handleSubmit(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+
+
+
+  axios.get(`http://localhost:8080/api/ia-knn/v1/calculate?xValue=${data.get('xvalue')}&yValue=${data.get('yvalue')}&kValue=${data.get('kvalue')}`)
+  .then(res => {
+    const knn = res.data;
+    console.log(knn)
+    this.setState({ knn });
+  })
+}
+  render(){ 
   return (
-    <div className="App">
+    <div className="landing">
       <div className="row">
-          <div className="col">
-            <h1>Knn Algorithm</h1>
+          <div className="col algorithm">
+            <div>Knn Algorithm</div>
           </div>
-          <div className="col">
-            <Form></Form>
+          <div className="col form">
+            <form onSubmit={this.handleSubmit}>
+            <div className="form-group row">
+                <label className="col-sm-2 col-form-label" htmlFor="xvalue">Enter X Value</label>
+                <input className='col-sm-3' id="xvalue" name="xvalue" type="number" />
+            </div>
+            <div className="form-group row">
+                <label className="col-sm-2 col-form-label" htmlFor="yvalue">Enter Y Value</label>
+                <input className='col-sm-3' id="yvalue" name="yvalue" type="number" />
+            </div>
+            <div className="form-group row">
+                <label className="col-sm-2 col-form-label" htmlFor="kvalue">Enter K Value</label>
+                <input min="1" className='col-sm-3' id="kvalue" name="kvalue" type="number" />
+            </div>
+            <div className="form-group row">
+                <button type='submit' className='btn btn-primary'>Calculate</button>
+            </div>
+          
+        </form>
           </div>
+          
       </div>
-      <Chart className='d-none'/>
+
+      <Charts className='d-none'/>
     </div>
   )
 }
-
+}
 export default App
